17671298ba6eca7a8b452c560723eded06d8a18b
djl12312 <96dalee@gmail.com>
2026-01-20 05:09:33 +0900

    Title: 달에서 무게 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260120/달에서 무게 구하기/weight-on-the-moon.java
+++ 260120/달에서 무게 구하기/weight-on-the-moon.java
@@ -3,10 +3,9 @@ public class Main {
         // Please write your code here.
 
         int a = 13;
-        boolean b = 0.165;
+        double b = 0.165;
 
-        
-
+    
         System.out.println(a + " * " + b + " = " = a * b);
     }
 }
