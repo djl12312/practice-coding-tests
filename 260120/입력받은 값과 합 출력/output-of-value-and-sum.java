b851c4226c291ac35af34c4395494847591d6131
djl12312 <96dalee@gmail.com>
2026-01-20 17:21:46 +0900

    Title: 입력받은 값과 합 출력, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260120/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        int A, B;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        
+        
+        System.out.println();
+        System.out.println();
+    }
+}
