f8fd6308fea52b5cb023f302a12f68aedc3db394
djl12312 <96dalee@gmail.com>
2026-01-20 19:23:24 +0900

    Title: 비교에 따른 연산, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 260120/비교에 따른 연산/operation-based-on-comparison.java
+++ 260120/비교에 따른 연산/operation-based-on-comparison.java
@@ -12,9 +12,7 @@ public class Main {
         if (a >= b) {
             System.out.println(a * b);
             } else {
-                System.out.println(b % a);
-            }
-
-
+                System.out.println(b / a);
+        }
     }
 }
