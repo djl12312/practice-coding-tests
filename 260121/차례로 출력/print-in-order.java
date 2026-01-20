a3af70527dacd31106a6f375df0d73933b96f2ac
djl12312 <96dalee@gmail.com>
2026-01-21 08:48:17 +0900

    Title: 차례로 출력, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260121/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for (int n = 5; n <= 17; n++)
+        {
+            System.out.print(n + " ");
+        }
+    }
+}
