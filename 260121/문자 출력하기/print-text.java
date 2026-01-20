d665ac9f98f45a199d6d7051cfb5158c5d95d3cd
djl12312 <96dalee@gmail.com>
2026-01-21 08:34:48 +0900

    Title: 문자 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260121/문자 출력하기/print-text.java
+++ 260121/문자 출력하기/print-text.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         char c;
 
-        n = next().charAt(0);
+        c = next().charAt(0);
 
         for (int i = 1; i <= 8; i++) {
             System.out.print(c);
