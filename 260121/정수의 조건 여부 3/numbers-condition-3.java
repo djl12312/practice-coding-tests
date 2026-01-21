753b0903898694a8cbbabbcad6f17a380c8ceebe
djl12312 <96dalee@gmail.com>
2026-01-21 17:42:23 +0900

    Title: 정수의 조건 여부 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260121/정수의 조건 여부 3/numbers-condition-3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A;
+    
+
+        A = sc.nextInt();
+        
+        if ( A % 13 = 0 || A % 19 = 0 ) {
+
+            System.out.print("True");
+
+        } else {
+            System.out.print("False");
+        }
+
+
+
+    }
+}
