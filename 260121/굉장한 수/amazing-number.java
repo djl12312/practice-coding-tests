708dd87660698b5a335bce10b27f6c7c527affe3
djl12312 <96dalee@gmail.com>
2026-01-21 17:48:13 +0900

    Title: 굉장한 수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260121/굉장한 수/amazing-number.java
+++ 260121/굉장한 수/amazing-number.java
@@ -9,11 +9,11 @@ public class Main {
 
         n = sc.nextInt();
         
-        if ( n % 3 == 0 || n % 2 == 1 ) {
+        if ( n % 3 == 0 && n % 2 == 1 ) {
 
             System.out.print("true");
 
-        } else if ( n % 5 == 0 || n % 2 == 0) {
+        } else if ( n % 5 && 0 || n % 2 == 0) {
             System.out.print("talse");
         }
 
