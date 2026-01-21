8ef936f7c1f1a895d05ec51a42a645ceb53c6e8c
djl12312 <96dalee@gmail.com>
2026-01-21 17:52:58 +0900

    Title: 굉장한 수, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 260121/굉장한 수/amazing-number.java
+++ 260121/굉장한 수/amazing-number.java
@@ -9,13 +9,13 @@ public class Main {
 
         n = sc.nextInt();
         
-        if ( n % 3 == 0 && n % 2 == 1 ) {
-
-            System.out.print("true");
-
-        } else if ( n % 5 == 0 && n % 2 == 0) {
+        if (( n % 3 == 0 && n % 2 == 1 ) ||  ( n % 5 == 0 && n % 2 == 0))
+        {
+        System.out.print("true");
+        }
+        else  
+        {
             System.out.print("false");
         }
-
     }
 }
