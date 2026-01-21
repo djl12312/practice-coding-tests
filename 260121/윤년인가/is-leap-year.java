51c8726a54b053f2a763f9f94c88d575b72cdeca
djl12312 <96dalee@gmail.com>
2026-01-21 18:10:30 +0900

    Title: 윤년인가, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260121/윤년인가/is-leap-year.java
+++ 260121/윤년인가/is-leap-year.java
@@ -8,9 +8,9 @@ public class Main {
 
         int Y;
 
-        Y = scs.nextInt();
+        Y = sc.nextInt();
 
-        if ( Y % 4 == 0) || (Y % 100 == 0 && Y % 400 |= 0) {
+        if (( Y % 4 == 0) || (Y % 100 == 0 && Y % 400 == 0)) {
             System.out.print("true");
 
         } else {
