36cd1b94d91437f172f8b5b819de6c60f6dd80f8
djl12312 <96dalee@gmail.com>
2026-01-21 17:47:57 +0900

    Title: 굉장한 수, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260121/굉장한 수/amazing-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+    
+
+        n = sc.nextInt();
+        
+        if ( n % 3 == 0 || n % 2 == 1 ) {
+
+            System.out.print("true");
+
+        } else if ( n % 5 == 0 || n % 2 == 0) {
+            System.out.print("talse");
+        }
+
+    }
+}
