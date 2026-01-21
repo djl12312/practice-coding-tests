72ff82e85705eead11092e6c21c40bee77512b7c
djl12312 <96dalee@gmail.com>
2026-01-21 19:32:04 +0900

    Title: 배열의 합 계산하기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260121/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int [] numbers = {3,1,4,5,9};
+        System.out.println(numbers[1] + numbers[3] + numbers[4]);
+
+    }
+}
