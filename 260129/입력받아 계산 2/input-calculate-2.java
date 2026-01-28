f0f75489618172b31b10ba5b41a870a316ce8cc7
djl12312 <96dalee@gmail.com>
2026-01-29 00:23:34 +0900

    Title: 입력받아 계산 2, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 260129/입력받아 계산 2/input-calculate-2.java
+++ 260129/입력받아 계산 2/input-calculate-2.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a, b;
+        int a;
+        int b;
         a = sc.nextInt();
         b = sc.nextInt();
 
-        System.out.println(a + b);
+        System.out.println(a * b);
     }
 }
