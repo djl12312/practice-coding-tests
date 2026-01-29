3cf4e13c7dccc264b37fcb67f97834b973ffd6e1
djl12312 <96dalee@gmail.com>
2026-01-29 10:20:51 +0900

    Title: 실수와 문자 받아 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260129/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        double a, b = sc.nextDouble();
+        char c = sc.next().charAt();
+
+        System.out.println(c);
+        System.out.println(c);
+
+        
+    }
+}
