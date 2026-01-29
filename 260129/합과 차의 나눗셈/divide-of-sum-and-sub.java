ecf4b9ce4b7f5999b1d746d54c9ab0af53223fb8
djl12312 <96dalee@gmail.com>
2026-01-29 16:45:40 +0900

    Title: 합과 차의 나눗셈, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260129/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+         sum = a + b;
+         minus = a + b;
+
+
+        System.out.printf("%.2f", (sum/minus));
+    }
+}
