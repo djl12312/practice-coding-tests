4bb7ccd1f0cced7f17cd6d6e8fbff0dddbd429ea
djl12312 <96dalee@gmail.com>
2026-01-29 19:06:12 +0900

    Title: 시험 통과 여부 확인하기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 260129/시험 통과 여부 확인하기/verify-test-passed.java
+++ 260129/시험 통과 여부 확인하기/verify-test-passed.java
@@ -4,14 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        // 변수 선언
+        int n;
         
-
-            if (n > 80) {
-                
-                System.out.println("pass");
-            } else {
-                System.out.println((80 - n) + " more score");
-            }            
+        // 입력
+        n = sc.nextInt();
+        
+        // 출력
+        if(n >= 80)
+            System.out.println("pass");
+        else
+            System.out.println((80 - n) + " more score");
     }
 }
